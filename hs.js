17bd5f6c857197cae1a849ac665048ae852d08e7
julien <BhnXnSYySHDirr5@BhnXnSYySHDirr5.BhnXnSYySHDirr5>
2016-06-26 11:14:48 +0200

    new hotshell commands

--- hs.js
+++ hs.js
@@ -1,4 +1,5 @@
 item({desc: 'julienmoumne.github.io'}, function () {
 
-    item({key: 's', cmd: 'jekyll serve'})
+    item({key: 's', cmd: 'jekyll serve --trace'})
+    item({key: 'u', desc: 'update GitHub Pages gem', cmd: 'bundle update'})
 })
