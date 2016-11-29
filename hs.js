08407b858cadad3359b785a10c58c767ee7e81b3
julien <BhnXnSYySHDirr5@BhnXnSYySHDirr5.BhnXnSYySHDirr5>
2016-11-29 18:41:43 +0100

    feat(hotshell): open website locally

--- hs.js
+++ hs.js
@@ -1,5 +1,9 @@
 item({desc: 'julienmoumne.github.io'}, function () {
 
+    var linux = exec('uname').indexOf('Linux') > -1
+    var browser = linux ? 'sensible-browser' : 'open'
+
     item({key: 's', cmd: 'bundle exec jekyll serve --trace'})
     item({key: 'u', desc: 'update GitHub Pages gem', cmd: 'bundle update'})
+    item({key: 'o', desc: 'open', cmd: browser + ' http://127.0.0.1:4000/'})
 })
